40fb37f0a84b337d20d2c71f1976359b9cfcd4bf
Radowan Hossain Sabbir <63876553+MrWhiteShadow@users.noreply.github.com>
2021-01-13 13:31:51 -0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "contact";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if($conn){
+    //echo "Connection OK";
+}
+else
+{
+    echo "Connection Error".mysqli_connect_error();
+}
+?>
